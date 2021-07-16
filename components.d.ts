6e78b54d5819401557eb6f5abf720ab060269975
Damian Tarnawski @thetarnav <gthetarnav@gmail.com>
2021-07-16 13:55:25 +0200

    remove layer0

--- components.d.ts
+++ components.d.ts
@@ -2,29 +2,19 @@
 // read more https://github.com/vuejs/vue-next/pull/3399
 
 declare module 'vue' {
-  export interface GlobalComponents {
-    AboutModal: typeof import('./src/components/AboutModal.vue')['default']
-    ExploreContainer: typeof import('./src/components/ExploreContainer.vue')['default']
-    IonAvatar: typeof import('@ionic/vue')['IonAvatar']
-    IonBackButton: typeof import('@ionic/vue')['IonBackButton']
-    IonButton: typeof import('@ionic/vue')['IonButton']
-    IonButtons: typeof import('@ionic/vue')['IonButtons']
-    IonChip: typeof import('@ionic/vue')['IonChip']
-    IonContent: typeof import('@ionic/vue')['IonContent']
-    IonHeader: typeof import('@ionic/vue')['IonHeader']
-    IonIcon: typeof import('@ionic/vue')['IonIcon']
-    IonLabel: typeof import('@ionic/vue')['IonLabel']
-    IonMenuButton: typeof import('@ionic/vue')['IonMenuButton']
-    IonPage: typeof import('@ionic/vue')['IonPage']
-    IonRouterOutlet: typeof import('@ionic/vue')['IonRouterOutlet']
-    IonSearchbar: typeof import('@ionic/vue')['IonSearchbar']
-    IonTabBar: typeof import('@ionic/vue')['IonTabBar']
-    IonTabButton: typeof import('@ionic/vue')['IonTabButton']
-    IonTabs: typeof import('@ionic/vue')['IonTabs']
-    IonTitle: typeof import('@ionic/vue')['IonTitle']
-    IonToolbar: typeof import('@ionic/vue')['IonToolbar']
-    Tabs: typeof import('./src/components/common/Tabs.vue')['default']
-  }
+	export interface GlobalComponents {
+		IonButton: typeof import('@ionic/vue')['IonButton']
+		IonButtons: typeof import('@ionic/vue')['IonButtons']
+		IonChip: typeof import('@ionic/vue')['IonChip']
+		IonContent: typeof import('@ionic/vue')['IonContent']
+		IonHeader: typeof import('@ionic/vue')['IonHeader']
+		IonIcon: typeof import('@ionic/vue')['IonIcon']
+		IonLabel: typeof import('@ionic/vue')['IonLabel']
+		IonPage: typeof import('@ionic/vue')['IonPage']
+		IonSearchbar: typeof import('@ionic/vue')['IonSearchbar']
+		IonTitle: typeof import('@ionic/vue')['IonTitle']
+		IonToolbar: typeof import('@ionic/vue')['IonToolbar']
+	}
 }
 
-export { }
+export {}
