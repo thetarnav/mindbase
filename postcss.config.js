25a833f182f6eedc747691410599c20cc1df69cf
Damian Tarnawski @thetarnav <gthetarnav@gmail.com>
2021-07-17 14:07:14 +0200

    postcss file fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,24 +1,23 @@
 // PostCSS configuration
 module.exports = cfg => {
-  // import tokens as Sass variables
-  // const variables = require('./tokens.json');
+	// import tokens as Sass variables
+	// const variables = require('./tokens.json');
 
-  const dev = cfg.env === "development",
-    scss = cfg.file?.extname === ".scss"
+	const dev = cfg.env === 'development'
 
-  return {
-    map: dev ? { inline: false } : false,
-    parser: "postcss-scss",
-    plugins: [
-      require("tailwindcss"),
-      require("postcss-advanced-variables"),
-      require("postcss-map-get"),
-      require("postcss-nested"),
-      require("postcss-sort-media-queries"),
-      // require('postcss-assets')({
-      //   loadPaths: ['src/images/']
-      // }),
-      require("autoprefixer"),
-    ],
-  }
+	return {
+		map: dev ? { inline: false } : false,
+		parser: 'postcss-scss',
+		plugins: [
+			require('tailwindcss'),
+			require('postcss-advanced-variables'),
+			require('postcss-map-get'),
+			require('postcss-nested'),
+			require('postcss-sort-media-queries'),
+			// require('postcss-assets')({
+			//   loadPaths: ['src/images/']
+			// }),
+			require('autoprefixer'),
+		],
+	}
 }
