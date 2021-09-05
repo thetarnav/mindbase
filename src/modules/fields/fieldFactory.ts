4ab436a50c956f41cdba88e3d6f4fb074eba6cc2
thetarnav <gthetarnav@gmail.com>
2021-09-05 18:10:20 +0200

    removed ContentNote Field

--- src/modules/fields/fieldFactory.ts
+++ src/modules/fields/fieldFactory.ts
@@ -2,13 +2,11 @@ import { FieldSettings, FieldType, RawFieldValues } from './types'
 import NumberFieldController from './fields/number/NumberFieldController'
 import TextFieldController from './fields/text/TextFieldController'
 import PhoneFieldController from './fields/phone/PhoneFieldController'
-import ContentNoteController from './fields/note/ContentNoteController'
 
 const fieldControllersMap = {
 	text: TextFieldController,
 	number: NumberFieldController,
 	phone: PhoneFieldController,
-	note: ContentNoteController,
 }
 export type FieldControllerGeneric<T extends FieldType> = InstanceType<
 	typeof fieldControllersMap[T]
@@ -20,7 +18,6 @@ const componentImport: Record<FieldType, ComponentImport> = {
 	text: () => import('./fields/text/TextField.vue') as any,
 	number: () => import('./fields/number/NumberField.vue') as any,
 	phone: () => import('./fields/phone/PhoneField.vue') as any,
-	note: () => import('./fields/note/ContentNote.vue') as any,
 }
 
 type ComponentImport = () => Promise<typeof import('*.vue')>
@@ -28,9 +25,7 @@ type ComponentImport = () => Promise<typeof import('*.vue')>
 export function createNewFieldController<T extends FieldType>(
 	type: T,
 ): FieldControllerGeneric<T> {
-	const controller = new fieldControllersMap[
-		type
-	]() as FieldControllerGeneric<T>
+	const controller = new fieldControllersMap[type]() as FieldControllerGeneric<T>
 	return controller
 }
 
